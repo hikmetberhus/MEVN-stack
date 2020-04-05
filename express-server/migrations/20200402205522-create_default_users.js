deba9a5bfc2a095a09184c686386c15723fa7e6e
hikmeberhus <hikmetberhus@gmail.com>
2020-04-05 05:18:30 +0300

    Users migration file modified.

--- express-server/migrations/20200402205522-create_default_users.js
+++ express-server/migrations/20200402205522-create_default_users.js
@@ -2,46 +2,45 @@ const bcrypt = require('bcryptjs')
 
 const User = require('../models/Users')
 
-let password = bcrypt.hashSync('123456', 10)
-
-const admin = new User(
-    {
-      email: 'admin@mail.com',
-      name: 'hikmet',
-      surname: 'berhus',
-      password: password,
-      permission: 'admin'
-    }
-)
-
-
-const user = new User(
-    {
-      email: 'user@mail.com',
-      name: 'hikmet',
-      surname: 'berhus',
-      password: password,
-      permission: 'user'
-    }
-)
-
-
 module.exports = {
   up(db) {
-      db.collection('users').insertOne(user)
-          .then((data) => {
-            console.log(data)
-          })
-          .catch((err) => {
-            console.log(err)
-          })
-    db.collection('users').insertOne(admin)
-        .then((data) => {
-          console.log(data)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+      bcrypt.hash('123456', 10).then((hash) => {
+          let admin = new User(
+              {
+                  email: 'admin@mail.com',
+                  name: 'hikmet',
+                  surname: 'berhus',
+                  password: hash,
+                  permission: 'admin'
+              }
+          )
+
+          let user = new User(
+              {
+                  email: 'user@mail.com',
+                  name: 'hikmet',
+                  surname: 'berhus',
+                  password: hash,
+                  permission: 'user'
+              }
+          )
+
+          db.collection('users').insertOne(user)
+              .then((data) => {
+                  console.log(data)
+              })
+              .catch((err) => {
+                  console.log(err)
+              })
+
+          db.collection('users').insertOne(admin)
+              .then((data) => {
+                  console.log(data)
+              })
+              .catch((err) => {
+                  console.log(err)
+              })
+      })
   },
 
   down(db) {
